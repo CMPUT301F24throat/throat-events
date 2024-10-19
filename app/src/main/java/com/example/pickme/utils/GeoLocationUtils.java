e5791bf95006c3bdd4671e5f9ce6210e3312982a
Sophia Cabungcal <sophiakrc@gmail.com>
2024-10-19 14:57:47 -0600

    Some initial planning

---
+++ app/src/main/java/com/example/pickme/utils/GeoLocationUtils.java
@@ -0,0 +1,13 @@
+package com.example.pickme.utils;
+
+/**
+ * Provides utilities for managing geolocation features
+ * Responsibilities:
+ * Check if geolocation is enabled
+ * Fetch and store user geolocation
+ * Validate geolocation data for event participation
+ * Calculate distance between two geolocations
+ **/
+
+public class GeoLocationUtils {
+}
---
+++ app/src/main/java/com/example/pickme/views/NotificationFragment.java
@@ -0,0 +1,11 @@
+package com.example.pickme.views;
+
+/**
+ * Displays the notifications for the user
+ * Responsibilities:
+ * Fetch notifications from the NotificationViewModel
+ * Allows the user to mark notifications as read or delete them
+ **/
+public class NotificationFragment {
+
+}
