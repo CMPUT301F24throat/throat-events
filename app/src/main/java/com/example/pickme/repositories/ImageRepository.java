1f4d743d75f0f6423386bdd0606017d1fac60428
Eric Dong <eric@donger.ca>
2024-10-22 15:32:36 -0600

    started on firebase implementation to upload images

--- app/src/main/java/com/example/pickme/repositories/ImageRepository.java
+++ app/src/main/java/com/example/pickme/repositories/ImageRepository.java
@@ -1,5 +1,14 @@
 package com.example.pickme.repositories;
 
+import android.util.Log;
+import com.example.pickme.models.Image;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import java.util.HashMap;
+
 /**
  * Handles image uploads and metadata storage
  * Responsibilities:
@@ -10,4 +19,40 @@ package com.example.pickme.repositories;
  **/
 
 public class ImageRepository {
+    //region Attributes
+
+    private final FirebaseStorage storage;
+    private final StorageReference storageRef;
+    private final FirebaseFirestore db;
+    private final CollectionReference imageCol;
+    //endregion
+
+    public ImageRepository() {
+        this.storage = FirebaseStorage.getInstance();
+        this.storageRef = storage.getReference();
+        this.db = FirebaseFirestore.getInstance();
+        this.imageCol = db.collection("images");
+    }
+
+    //region Methods
+    // unfinished
+    public void upload() {
+        Image image = new Image();
+
+        HashMap<String, Object> imgData = new HashMap<>();
+        imgData.put("imageUrl", "placeholder");
+        imgData.put("type", image.getType());
+        imgData.put("uploadedBy", image.getUploadedBy());
+        imgData.put("dateCreated", image.getDateCreated());
+        imageCol
+                .document()
+                .set(imgData)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("Firestore", "DocumentSnapshot successfully written!");
+                    }
+                });
+    }
+    //endregion
 }
