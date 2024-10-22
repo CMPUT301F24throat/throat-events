57de9d10d40ca48350178017ecfe409dad52b8c7
Eric Dong <eric@donger.ca>
2024-10-22 14:41:40 -0600

    added attributes, setters, and getters to image class

--- app/src/main/java/com/example/pickme/models/Image.java
+++ app/src/main/java/com/example/pickme/models/Image.java
@@ -1,5 +1,7 @@
 package com.example.pickme.models;
 
+import java.time.LocalDateTime;
+
 /**
  * Represents uploaded images (profile pictures, event posters)
  * Responsibilities:
@@ -8,4 +10,55 @@ package com.example.pickme.models;
  **/
 
 public class Image {
+    //region Attributes
+
+    private String imageUrl;
+    private imageType type;
+    private String uploadedBy;
+    private LocalDateTime dateCreated;
+
+    public enum imageType {
+        img_profile,
+        img_event
+    }
+    //endregion
+
+    //region Getters
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public imageType getType() {
+        return type;
+    }
+
+    public String getUploadedBy() {
+        return uploadedBy;
+    }
+
+    public LocalDateTime getDateCreated() {
+        return dateCreated;
+    }
+    //endregion
+
+    //region Setters
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public void setType(imageType type) {
+        this.type = type;
+    }
+
+    public void setUploadedBy(String uploadedBy) {
+        this.uploadedBy = uploadedBy;
+    }
+
+    public void setDateCreated(LocalDateTime dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+    //endregion
 }
+
