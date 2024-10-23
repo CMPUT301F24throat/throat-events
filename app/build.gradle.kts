b8a999e2921e3aed9650c2f349e17241dbb4447d
Cole M <cdmckay1@ualberta.ca>
2024-10-23 00:08:02 -0600

    Created QR code generator skeleton code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
+    implementation(libs.zxing.core)
+    implementation(libs.zxing.android)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/example/pickme/utils/QRCodeGenerator.java
+++ app/src/main/java/com/example/pickme/utils/QRCodeGenerator.java
@@ -1,5 +1,12 @@
 package com.example.pickme.utils;
 
+import android.graphics.Bitmap;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
 /**
  * Utility to generate QR codes for events
  * Responsibilities:
@@ -8,4 +15,48 @@ package com.example.pickme.utils;
  **/
 
 public class QRCodeGenerator {
+
+    private static final int QR_CODE_WIDTH = 500;
+    private static final int QR_CODE_HEIGHT = 500;
+
+    /**
+     * Generate QR Code Bitmap from a given event string
+     *
+     * @param eventData Data for the event (e.g., event ID or hash)
+     * @return Bitmap representing the QR code
+     */
+    public static Bitmap generateQRCode(String eventData) {
+        try {
+            QRCodeWriter writer = new QRCodeWriter();
+            BitMatrix bitMatrix = writer.encode(eventData, BarcodeFormat.QR_CODE, QR_CODE_WIDTH, QR_CODE_HEIGHT);
+
+            int width = bitMatrix.getWidth();
+            int height = bitMatrix.getHeight();
+            Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    bmp.setPixel(x, y, bitMatrix.get(x, y) ? 0xFF000000 : 0xFFFFFFFF);  // Black or White
+                }
+            }
+            return bmp;
+        } catch (WriterException e) {
+            // add logging
+            return null;
+        }
+    }
+
+    /**
+     * Convert event to a QR Code image URL
+     *
+     * @param eventData Data for the event
+     * @return URL of the generated QR code image (Placeholder: add actual upload/storage logic)
+     */
+    public static String getQRCodeImageURL(String eventData) {
+        // For now, this method is a placeholder.
+        Bitmap qrCodeBitmap = generateQRCode(eventData);
+
+        // TODO: Upload qrCodeBitmap to Firebase and return the URL/pointer to data
+        return "https://example.com/qrcode_placeholder.png";
+    }
 }
+
