e3470ffc73c50a632d5880f1ae5e58e0c48cb4e1
Eric Dong <eric@donger.ca>
2024-10-24 19:23:17 -0600

    working upload method

--- app/src/main/java/com/example/pickme/repositories/ImageRepository.java
+++ app/src/main/java/com/example/pickme/repositories/ImageRepository.java
@@ -34,8 +34,6 @@ public class ImageRepository {
     private final FirebaseFirestore db = FirebaseFirestore.getInstance();
     private final CollectionReference imgCollection = db.collection("images");
 
-    // authentication
-    private final FirebaseAuth auth = FirebaseAuth.getInstance();
     public ImageRepository() {
         // temporary anonymous auth
         FirebaseAuth auth = FirebaseAuth.getInstance();
@@ -49,6 +47,53 @@ public class ImageRepository {
         });
     }
 
-    // TODO: not sure how images work yet; need to figure out how to store them in Firebase Storage
+    // TODO: fix nesting due to concurrency issues,
+    //  separate methods for user/event uploads (once grabbing user/event id's becomes available)
+    /**
+     * Uploads an image URI to FirebaseStorage, then stores the image information to Firestore DB
+     * @param imgUri The image URI to upload (e.g. one obtained from an image picker)
+     */
+    public void upload(Uri imgUri) {
+        // creating document first to generate unique id
+        DocumentReference imgDoc = imgCollection.document();
+
+        // setting child storage reference to the unique id
+        StorageReference imgRef = imgStorage.child(imgDoc.getId());
+
+        // storing the image in firebasestorage
+        imgRef
+            .putFile(imgUri)
+            .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Log.d("ImageRepository", "STORAGE: URI " + imgUri.toString() + " upload successful");
+
+                    // now get the image download url...
+                    imgRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+
+                            // ... and create a new image object to store to DB
+                            // TODO: Change fields to user/event getter methods
+                            Image img = new Image(uri.toString(), ImageType.PROFILE_PICTURE, "user", "1234");
+
+                            // db store
+                            imgDoc
+                                .set(img.getUploadPackage())
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        Log.d("ImageRepository","DB: Upload successful");
+                                    }
+                                });
+                        }
+                    });
+                }
+            });
+    }
+
+    // TODO: finish download method
+    public void download() {
+    }
 
 }
