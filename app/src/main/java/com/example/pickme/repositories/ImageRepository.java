9e661026b2057abeb30b3ff26f1e570d55553b82
Eric Dong <eric@donger.ca>
2024-10-24 18:50:53 -0600

    added storage and auth references

--- app/src/main/java/com/example/pickme/repositories/ImageRepository.java
+++ app/src/main/java/com/example/pickme/repositories/ImageRepository.java
@@ -11,8 +11,16 @@ import com.google.firebase.firestore.FirebaseFirestore;
  * CRUD operations for image data
  */
 public class ImageRepository {
+    // image storage
+    private final FirebaseStorage storage = FirebaseStorage.getInstance();
+    private final StorageReference imgStorage = storage.getReference();
+
+    // database access
     private final FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private final CollectionReference imageRef = db.collection("images");
+    private final CollectionReference imgCollection = db.collection("images");
+
+    // authentication
+    private final FirebaseAuth auth = FirebaseAuth.getInstance();
 
     // TODO: not sure how images work yet; need to figure out how to store them in Firebase Storage
 
