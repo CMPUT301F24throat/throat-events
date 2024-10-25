e5fb328dd352f0060b3fd7366701bfb0b3ca9b84
Eric Dong <eric@donger.ca>
2024-10-24 20:00:56 -0600

    added user/event upload separation

--- app/src/main/java/com/example/pickme/views/Example_ImageUpload.java
+++ app/src/main/java/com/example/pickme/views/Example_ImageUpload.java
@@ -12,6 +12,8 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.pickme.R;
+import com.example.pickme.models.Event;
+import com.example.pickme.models.User;
 import com.example.pickme.repositories.ImageRepository;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -19,13 +21,17 @@ public class Example_ImageUpload extends AppCompatActivity {
 
     private FirebaseFirestore db;
 
+    User user1 = new User();
+    Event event1 = new Event();
+
     // this is the gallery picker promise/contract so we can grab the image uri
     ActivityResultLauncher<PickVisualMediaRequest> pickMedia =
             registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), uri -> {
                 // callback is invoked after the user selects a media item or closes the photo picker
                 if (uri != null) {
                     ImageRepository ir = new ImageRepository();
-                    ir.upload(uri);
+                    ir.upload(uri, user1);
+                    ir.upload(uri, user1, event1);
                 } else {
                     Log.d("PhotoPicker", "No media selected");
                 }
