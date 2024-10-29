8b143ae1ece0537a814c42f13c35d80ae493cec4
Aesoji <138826639+Aesoji@users.noreply.github.com>
2024-10-28 22:25:45 -0600

    Account Initialization and SignUp Feature (Almost done)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.firebase.inappmessaging)
+    implementation(libs.mediation.test.suite)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
