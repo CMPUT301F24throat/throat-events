4b552edcf385956a28c71e651f3c9a0a4debf845
Cole M <cdmckay1@ualberta.ca>
2024-10-29 21:17:06 -0600

    Added layout for the QR code viewer

---
+++ app/src/main/java/com/example/pickme/views/QRCodeViewFragment.java
@@ -0,0 +1,15 @@
+package com.example.pickme.views;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * Displays the QR code for an event
+ * Responsibilities:
+ * Display the large format QR code for an event
+ * Allow users to share the QR code
+ **/
+public class QRCodeViewFragment extends Fragment {
+
+}
+
+
