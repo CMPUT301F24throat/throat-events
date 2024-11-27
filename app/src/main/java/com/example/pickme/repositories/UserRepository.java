c517fab6d00885efaf51e4004a0bcd012e5ca7a5
Sophia Cabungcal <sophiakrc@gmail.com>
2024-11-27 16:27:18 -0700

    Update UserRepository.java

--- app/src/main/java/com/example/pickme/repositories/UserRepository.java
+++ app/src/main/java/com/example/pickme/repositories/UserRepository.java
@@ -13,8 +13,6 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.messaging.FirebaseMessaging;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 
 /**
